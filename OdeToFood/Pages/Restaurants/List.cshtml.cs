3be45c688793a2f136e9b7c5db662ed1ee38ef91
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2020-12-06 14:37:12 -0600

    taking advantage of PaegModel to access config data from appsettings

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
+
+    // ------------------------ PageModel -----------------------------------------------------------------------------------
+    // basically PageModel is used to inject services that will give me access to the data that I need
+    // and then use those services, like the configuration service, to fetch data and add that to properties 
+    // and then bind to inside of the razor page
+    //  ---------------------------------------------------------------------------------------------------------------------
+
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
         public string Message { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
 
         public void OnGet()
         {
-            Message = "Hello World!";
+            Message = config["Message"];
         }
     }
 }

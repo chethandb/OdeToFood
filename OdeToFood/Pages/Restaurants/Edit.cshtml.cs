e2de0a07137b52d781d5c640855e8a0f2f4edca4
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2020-12-09 22:43:49 -0600

    handling create vs update on onpost

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -20,7 +20,7 @@ namespace OdeToFood.Pages.Restaurants
         public Restaurant Restaurant { get; set; }
         public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData, 
+        public EditModel(IRestaurantData restaurantData,
                          IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
@@ -38,7 +38,7 @@ namespace OdeToFood.Pages.Restaurants
             else
             {
                 Restaurant = new Restaurant();
-            }            
+            }
 
             if (Restaurant == null)
             {
@@ -50,18 +50,26 @@ namespace OdeToFood.Pages.Restaurants
 
         public IActionResult OnPost()
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                Restaurant = restaurantData.Update(Restaurant);
-                restaurantData.Commit();
-                // implement post redirect get pattern - PRG 
-                // this makes sure user is left on a safe page where they can refresh and wouldn't change any data
-                return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+                // we have to make sure to populate the select list for Post as well 
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
             }
 
-            // we have to make sure to populate the select list for Post as well 
-            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();            
-            return Page();
+            if (Restaurant.Id > 0)
+            {
+                Restaurant = restaurantData.Update(Restaurant);
+            }
+            else
+            {
+                restaurantData.Add(Restaurant);
+            }
+
+            restaurantData.Commit();
+            // implement post redirect get pattern - PRG 
+            // this makes sure user is left on a safe page where they can refresh and wouldn't change any data
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
         }
     }
 }

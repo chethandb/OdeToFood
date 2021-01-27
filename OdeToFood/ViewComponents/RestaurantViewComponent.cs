edc4cab31a55695350e570beb6743a619272d31d
Chethan D. Balakrishna <Chethan.Balakrishna@biberk.com>
2021-01-26 23:35:18 -0600

    changes for view component, need to debug to make sure view componenet is working, pending

---
+++ OdeToFood/ViewComponents/RestaurantViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class RestaurantViewComponent : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestaurantViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetCountOfRestaurants();
+
+            // if no name is provided for the view it defaults            
+            return View(count);
+        }
+    }
+}
